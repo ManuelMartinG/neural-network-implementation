5eabab609b9a41c13f0848b2be8e72e2945c2384
manuel.martin5.next <manuel.martin5.next@bbva.com>
2020-06-19 19:49:08 +0200

    feat: working version of neuralnet class + example on notebook

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+## Neural Network Notes
+
+[Initialize Weights Tutorial (Deep Learning.ai)](https://www.deeplearning.ai/ai-notes/initialization/)
--- neuralnet.py
+++ neuralnet.py
@@ -1,10 +1,9 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
-from sklearn.datasets import make_blobs
 
 
-class Maths:
+class Functions:
+    """TODO: Put this in a separate module"""
     @staticmethod
     def he_initialize(shape):
         he = np.random.normal(loc=0, scale=np.sqrt(1 / shape[0]))
@@ -23,84 +22,90 @@ class Maths:
         return 1 / (1 + np.exp(-z))
 
 
+f = Functions
+
+
 class NeuralNet:
-    
-    def __init__(self, layers, **hyperparams):
-        self.m = Maths()
-        self.layers_sizes = [self.m.set_bias_as_weight(sh) for sh in layers]
-        self.layers = self.initialize_layer_weights()
-        self.lr = hyperparams.get("lr", 0.01)
-        
-    def initialize_layer_weights(self):
+
+    __slots__ = ["layer_shapes", "hyper_params", "layers", "lr"]
+
+    def __init__(self, layer_shapes):
+        self.layer_shapes = [f.set_bias_as_weight(w) for w in layer_shapes]
+        self.layers = self.init_weights("he")
+        self.lr = 0.002
+
+    def init_weights(self, method="he"):
+        """TODO: Support Xavier, Uniform, Normal, etc."""
         layers = []
-        for size in self.layers_sizes:
-            layers.append(self.m.he_initialize(size))
+        for shape in self.layer_shapes:
+            weights = f.he_initialize(shape)
+            layers.append(weights)
         return layers
 
-    def _forward_layer(self, layer, v):
-        return self.m.sigmoid(np.dot(self.m.add_bias(v), layer))
+    @staticmethod
+    def _forward_layer_pass(layer_weights, vector):
+        """Forward Propagation over a specific Layer and Vector"""
+        return f.sigmoid(np.dot(f.add_bias(vector), layer_weights))
 
-    def get_layer_forwards(self, x):
-        layer_forwards = [x]
+    def compute_forward_propagation(self, x):
+        """Compute Full Forward Propagation"""
+        layer_outputs = [x]
         for layer in self.layers:
-            x = self._forward_layer(layer, x)
-            layer_forwards.append(x)
-        return layer_forwards
-    
-    def backward(self, x, ytrue):
-        m = x.shape[0]
-        forwards = self.get_layer_forwards(x)
-        
-        # Output Layer Error
-        d = (forwards[-1] - ytrue) * forwards[-1] * (1 - forwards[-1])
-        wv = (1 / m) * np.dot(d.T, self.m.add_bias(forwards[-2]))
-        
-        ds = [d]
+            x = self._forward_layer_pass(layer, x)
+            layer_outputs.append(x)
+        return layer_outputs
+
+    @staticmethod
+    def compute_output_layer_error(y_pred, y_true):
+        d = (y_pred - y_true) * y_pred * (1 - y_pred)
+        return d
+
+    def compute_backward_propagation(self, x, y_true):
+        """Compute full Backward Propagation"""
+        m = x.shape[0]  # m -> Number of training samples
+        forwards = self.compute_forward_propagation(x)
+
+        # Compute error in output layer
+        d = self.compute_output_layer_error(forwards[-1], y_true)
+        wv = (1 / y_true.shape[0]) * np.dot(d.T, f.add_bias(forwards[-2]))
         wvs = [wv.T]
-        
-        for l in range(len(self.layers) - 1):
-            d = np.dot(d, self.layers[-l - 1].T) * \
-                   self.m.add_bias(forwards[-l - 2]) * \
-                   (1 - self.m.add_bias(forwards[-l - 2]))
+
+        # Compute weights variations based on error propagations
+        for li in range(len(self.layers) - 1):
+            d = np.dot(d, self.layers[-li - 1].T) * \
+                f.add_bias(forwards[-li - 2]) * \
+                (1 - f.add_bias(forwards[-li - 2]))
             d = d[:, :-1]
-            wv = (1/m) * np.dot(d.T, self.m.add_bias(forwards[-l - 3]))
-            ds.insert(0, d)
-            wvs.insert(0, wv.T)
+            wv = (1 / m) * np.dot(d.T, f.add_bias(forwards[-li - 3]))
+            wvs.insert(0, wv)
         return wvs
-    
-    def infer(self, x):
-        for layer in self.layers:
-            x = self._forward_layer(layer, x)
-        return x
-    
+
     def train(self, x, y, epochs, batch_size):
         y = y.reshape(-1, 1)
         m = x.shape[0]
-        n_batches = int(np.floor(m / batch_size))
-        rem_batch_size = m - n_batches * batch_size
-        
-        for _ in tqdm.tqdm(range(epochs)):
-            batched_weights = []
-            new_weights = []
-            accumulated_weights = [0] * len(self.layers)
-            
-            for n in range(n_batches):
-                
-                x_ = x[n * batch_size: (n + 1)* batch_size]
-                y_ = y[n * batch_size: (n + 1) * batch_size]
+        n_batches = int(np.ceil(m / batch_size))
 
-                wvs = self.backward(x_, y_)
+        # Iterate training by each epoch
+        for epoch in range(epochs):
+            batched_weights = []
+            updated_weights = []
+            accumulated_weights = [0] * len(self.layers)
+
+            for n in tqdm.tqdm(range(n_batches)):
+                # Get X and Y batches
+                _x_batch = x[n * batch_size: (n + 1) * batch_size, :]
+                _y_batch = y[n * batch_size: (n + 1) * batch_size]
+
+                # Calculate backward propagation
+                wvs = self.compute_backward_propagation(_x_batch, _y_batch)
                 batched_weights.append(wvs)
-            
-            # Sum all weights variations for each batch
-            for i in range(0, len(batched_weights)):
-                for j in range(len(batched_weights[i])):
-                    accumulated_weights[j] = accumulated_weights[j] + batched_weights[i][j]
-            
-            # For the weights in each accumulated batched weights variations, apply update
-            for weigths, wv in zip(self.layers, accumulated_weights):
-                weigths = weigths - self.lr * wv
-                new_weights.append(weigths)
-            self.layers = new_weights
-        return self.layers
-    
+
+            for bw in range(len(batched_weights)):
+                # bw -> List of lists with all weight matrices
+                for wm in range(len(batched_weights[bw])):
+                    accumulated_weights[wm] += batched_weights[bw][wm]
+
+            for weights, wv in zip(self.layers, accumulated_weights):
+                weights = weights - self.lr * wv.T
+                updated_weights.append(weights)
+            self.layers = updated_weights
