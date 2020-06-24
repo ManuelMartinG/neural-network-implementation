3d39c6431c6764c5d70d806e142f7ee09aba19d2
manuel.martin5.next <manuel.martin5.next@bbva.com>
2020-06-24 03:37:41 +0200

    feat: working solution for dynamical decision function as a meshgrid

--- neuralnet/neuralnet.py
+++ neuralnet/neuralnet.py
@@ -1,114 +1,254 @@
 import numpy as np
-import functions as f
+import time
+import neuralnet.functions as f
 
-np.random.seed(10)
+try:
+    import matplotlib.pyplot as plt
+    from IPython import display
+    pass
+except Exception as ex:
+    ImportError(ex)
+
+
+class FullyConnectedLayer:
+    def __init__(self, shape_in, shape_out, activation):
+        self.shape = (shape_in, shape_out)
+
+        if activation == "sigmoid":
+            self.activation = f.sigmoid
+        else:
+            raise ValueError(f"""{activation} not currently supported.
+                             Try with `sigmoid`""")
+
+        self.weights = None  # To be initialized by NeuralNet
+
+    def initialize_weights(self, method="he"):
+        if method == "he":
+            self.weights = f.he_initialize(self.shape)
+        else:
+            raise ValueError("Only supported He-Normal method")
+
+    def forward(self, x):
+        z = np.dot(f.add_bias(x), self.weights)
+        return self.activation(z)
+
+    def backpropagate(self):
+        pass
+
+
+class LayersChain():
+    def __init__(self, layers_list):
+        self.chain = layers_list
+
+    def __repr__(self):
+        repr = "Neural Network Architecture\n" + \
+               "===========================\n\n"
+        for i, layer in enumerate(self.chain):
+            layer_name = layer.__class__.__name__
+            shapes = layer.shape
+            repr += f"\n> Layer {i} - {layer_name} ({shapes[0]}, {shapes[1]})\n"
+        return repr
+
+    def __getitem__(self, index):
+        return self.chain[index]
+
+    def __len__(self):
+        return len(self.chain)
+
+    @property
+    def weights(self):
+        return [l.weights for l in self.chain]
+
+    def update_weights(self, new_weights):
+        new_chain = []
+        for layer, new_weights in zip(self.chain, new_weights):
+            layer.weights = new_weights
+            new_chain.append(layer)
+        self.chain = new_chain
+
+
+class OptimizationHistory:
+    def __init__(self, activate_mesh_history=False):
+        self.mean_error_per_epoch = []
+        self.weight_variations_per_epoch = []
+
+        if activate_mesh_history:
+            self.mesh_predictions_per_epoch = []
+
+    def add_error(self, error):
+        self.mean_error_per_epoch.append(error)
+
+    def add_weigth_diff(self, epoch, weight_diff):
+        self.weight_variations_per_epoch[epoch].append(weight_diff)
+
+
+class OptimizationVisuals:
+
+    def __init__(self, x, y):
+        self. n = 256
+        self.xmin = x.min()
+        self.xmax = x.max()
+        self.ymin = y.min()
+        self.ymax = y.max()
+        self.x = np.linspace(self.xmin, self.xmax, self.n)
+        self.y = np.linspace(self.ymin, self.ymax, self.n)
+        self.X, self.Y = np.meshgrid(self.x, self.y)
+        self.positions = np.hstack(
+            [self.X.flatten().reshape(-1, 1),
+             self.Y.flatten().reshape(-1, 1)])
+
+    def print_mesh(self, forward_prop_function):
+        Z = forward_prop_function(self.positions)[-1]
+        print(Z.mean())
+        plt.gca().cla()
+        plt.xlim((self.xmin, self.xmax))
+        plt.ylim((self.ymin, self.ymax))
+        plt.pcolormesh(self.X, self.Y, Z.reshape(256, 256), cmap=plt.cm.viridis)
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
+        time.sleep(0.001)
+
+    def decision_function_subplots(self):
+        # TODO: Mostrar sólo 6 subplots dividiendo las epochs entre 6. Si hay menos epochs, se muestran
+        # todas las que haya (pares)
+        pass
 
 
 class NeuralNet:
 
-    __slots__ = ["layer_shapes", "layers", "lr", "__fitted_once"]
-
     def __init__(self, layer_shapes):
-        self.layer_shapes = [f.set_bias_as_weight(w) for w in layer_shapes]
-        print(self.layer_shapes)
-        self.layers = self.init_weights("he")
-        self.lr = 0.002
-        self.__fitted_once = False
+        self._no_bias_layer_shapes = layer_shapes
+        self.layer_shapes = self._get_biased_absorbed_shapes(
+                self._no_bias_layer_shapes)
+        self.layers = self._build_layers()
+        self.ophist = self._set_optim_history()
+        self.training_mandatory_arguments = ["learning_rate", "batch_size"]
+        self.__fitted__ = False
 
-    def init_weights(self, method="he"):
-        """TODO: Support Xavier, Uniform, Normal, etc."""
+    def _set_optim_history(self):
+        return OptimizationHistory(activate_mesh_history=True)
+
+    def _build_layers(self):
         layers = []
         for shape in self.layer_shapes:
-            weights = f.he_initialize(shape)
-            layers.append(weights)
-        return layers
+            fcl = FullyConnectedLayer(*shape, activation="sigmoid")
+            fcl.initialize_weights(method="he")
+            layers.append(fcl)
+        return LayersChain(layers)
 
-    @staticmethod
-    def _forward_layer_pass(layer_weights, vector):
-        """Forward Propagation over a specific Layer and Vector"""
-        return f.sigmoid(np.dot(f.add_bias(vector), layer_weights))
+    def _get_biased_absorbed_shapes(self, layer_shapes):
+        return [f.set_bias_as_weight(s) for s in layer_shapes]
 
-    def _restart_weights(self):
-        self.layers = self.init_weights("he")
+    def _maybe_init_for_re_run(self):
+        if self.__fitted__:
+            self.layers = self._build_layers()
+            self.ophist = self._set_optim_history()
 
-    def compute_forward_propagation(self, x):
-        """Compute Full Forward Propagation"""
+    def forward_propagation(self, x):
         layer_outputs = [x]
         for layer in self.layers:
-            x = self._forward_layer_pass(layer, x)
+            x = layer.forward(x)
             layer_outputs.append(x)
         return layer_outputs
 
+    def output_error(self, ypred, ytrue):
+        delta = (ypred - ytrue) * ypred * (1 - ypred)
+        return delta
+
+    def backward_propagation(self, x, ytrue):
+        m = x.shape[0]
+        forwards = self.forward_propagation(x)
+
+        # Get error on Output Layer (last layer)
+        error = delta = self.output_error(forwards[-1], ytrue)
+        output_weights_diff = (1/m) * np.dot(delta.T, f.add_bias(forwards[-2]))
+
+        all_weights_diff = [output_weights_diff]
+
+        # Get error on Hidden Layers (all layers except last one)
+        for layer_pos in range(len(self.layers) - 1):
+            delta = np.dot(delta, self.layers[-layer_pos - 1].weights.T) *\
+                    f.add_bias(forwards[-layer_pos - 2]) * (1 - f.add_bias(forwards[-layer_pos - 2]))
+
+            delta = delta[:, :-1]  # Avoid propagate bias
+
+            layer_weights_diff = (1/m) * np.dot(delta.T, f.add_bias(forwards[-layer_pos - 3]))
+            all_weights_diff.insert(0, layer_weights_diff)
+        return all_weights_diff, error
+
     @staticmethod
-    def compute_output_layer_error(y_pred, y_true):
-        d = (y_pred - y_true) * y_pred * (1 - y_pred)
-        return d
+    def show_progress(epoch, error_avg, weight_diffs, show_after=5):
+        if epoch % show_after == 0:
+            print(f"Epoch {epoch} | "
+                  + f"Error: {error_avg} | "
+                  + f"WV0: {weight_diffs[0]} | "
+                  + f"WV1: {weight_diffs[1]}")
 
-    def compute_backward_propagation(self, x, y_true):
-        """Compute full Backward Propagation"""
-        m = x.shape[0]  # m -> Number of training samples
-        forwards = self.compute_forward_propagation(x)
+    def validate_optimization_arguments(self, **kwargs):
+        for arg in self.training_mandatory_arguments:
+            if arg not in kwargs.keys():
+                raise ValueError(f"Argument {arg} is mandatory!")
 
-        # Compute error in output layer
-        error = d = self.compute_output_layer_error(forwards[-1], y_true)
-        wv = (1 / y_true.shape[0]) * np.dot(d.T, f.add_bias(forwards[-2]))
-        wvs = [wv]
+    def optimize(self, x, y, epochs, **kwargs):
+        # Validate input parameters and restart state if needed
+        self.validate_optimization_arguments(**kwargs)
+        self._maybe_init_for_re_run()
+        ovisuals = OptimizationVisuals(x=x[:, 0], y=x[:, 1])
 
-        # Compute weights variations based on error propagations
-        for li in range(len(self.layers) - 1):
-            d = np.dot(d, self.layers[-li - 1].T) * \
-                f.add_bias(forwards[-li - 2]) * \
-                (1 - f.add_bias(forwards[-li - 2]))
-            d = d[:, :-1]
-            wv = (1 / m) * np.dot(d.T, f.add_bias(forwards[-li - 3]))
-            wvs.insert(0, wv)
-        return wvs, error
-
-    def train(self, x, y, epochs, batch_size, learning_rate, stochastic=True):
-        if self.__fitted_once:
-            print("Restarting weights to randomized initialization...")
-            self._restart_weights()
-        self.lr = learning_rate
+        # Set training parameters
         y = y.reshape(-1, 1)
         m = x.shape[0]
+        learning_rate = kwargs.get("learning_rate", 0.002)
+        batch_size = kwargs["batch_size"]
         n_batches = int(np.ceil(m / batch_size))
+        progess_cadence = kwargs.get("show_after", 10)
+        self.ophist.weight_variations_per_epoch = [0] * epochs
 
-        # Iterate training by each epoch
         for epoch in range(epochs):
             batched_weights = []
             updated_weights = []
             accumulated_weights = [0] * len(self.layers)
-            accumulated_error = []
-            if stochastic:
+
+            # Shuffle samples for every epoch in order to perform stochastic optimization
+            if kwargs.get("stochastic", True):
                 x, y = f.shuffle_vectors(x, y)
 
             for n in range(n_batches):
-                # Get X and Y batches
-                _x_batch = x[n * batch_size: (n + 1) * batch_size, :]
-                _y_batch = y[n * batch_size: (n + 1) * batch_size]
+                x_batch = x[n * batch_size: (n + 1) * batch_size, :]
+                y_batch = y[n * batch_size: (n + 1) * batch_size]
 
-                # Calculate backward propagation
-                wvs, error = self.compute_backward_propagation(
-                    _x_batch, _y_batch)
-                accumulated_error.append(error.mean())
-                batched_weights.append(wvs)
+                weights_diff, error = self.backward_propagation(
+                        x_batch, y_batch)
 
+                # Save Mean Error and Weight Variations
+                self.ophist.add_error(error.mean())
+                batched_weights.append(weights_diff)
+
+            # Sum up for each weight matrices, their respective diffs for each batch
             for bw in range(len(batched_weights)):
-                # bw -> List of lists with all weight matrices
                 for wm in range(len(batched_weights[bw])):
                     accumulated_weights[wm] += batched_weights[bw][wm]
 
-            mean_weight_vars = []
-            for weights, wv in zip(self.layers, accumulated_weights):
-                weights_ = weights - self.lr * wv.T
-                mean_weight_vars.append(wv)
-                updated_weights.append(weights_)
+            self.ophist.weight_variations_per_epoch[epoch] = []
+            for weights, weights_diff in zip(self.layers.weights, accumulated_weights):
+                new_weights = weights - learning_rate * weights_diff.T
 
-            self.__fitted_once = True
-            self.layers = updated_weights
+                # Save Weight differences applied
+                self.ophist.add_weigth_diff(epoch, weights_diff)
 
-            error_avg = round(np.array(accumulated_error).mean(), 9)
-            wvar = [round(np.array(mwv).mean(), 9) for mwv in mean_weight_vars]
+                # Update new Neural Network Weights
+                updated_weights.append(new_weights)
 
-            epoch_msg = f"""Epoch {epoch} | Error: {error_avg} | WV0: {wvar[0]} | WV1: {wvar[1]}"""
-            print(epoch_msg)
+            self.layers.update_weights(updated_weights)
+            self.__fitted__ = True
+
+            if kwargs.get("visuals", True):
+                ovisuals.print_mesh(self.forward_propagation)
+
+            error_avg = round(np.array(self.ophist.mean_error_per_epoch).mean(), 9)
+            wvar = [round(np.array(mwv).mean(), 9) for mwv in self.ophist.weight_variations_per_epoch[epoch]]
+
+            self.show_progress(epoch=epoch, error_avg=error_avg,
+                               weight_diffs=wvar, show_after=progess_cadence)
+        if kwargs.get("visuals", True):
+            plt.show()
